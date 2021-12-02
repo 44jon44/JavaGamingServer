8969bcb0fd5e79127808d446949876b65008a8d1
hola <hola@gmail.com>
2021-12-02 10:29:49 +0100

    se han generado las diferentes Exceptions

---
+++ src/java/exception/LoginNotFoundException.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exception;
+/**
+ *Excepcion que se señala que el usuario no esta registrado
+* @author Ibai,jon,markel,alex
+ */
+public class LoginNotFoundException extends Exception {
+
+    /**
+     * Crea una instancia de <code>LoginNotFoundException</code> sin mensaje de
+     * detalle.
+     */
+    public LoginNotFoundException() {
+        super("El usuario introducido no existe");
+    }
+}
---
+++ src/java/exception/RepeatPasswordException.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exception;
+
+/**
+ * Excepcion que se señala que las contraseñas no coinciden
+ *
+ * @author Ibai,jon,markel,alex
+ */
+public class RepeatPasswordException extends Exception {
+
+    /**
+     * Crea una instancia de <code>RepeatPasswordException</code> sin mensaje de
+     * detalle.
+     */
+    public RepeatPasswordException() {
+        super("Las contraseñas no coinciden");
+    }
+}
