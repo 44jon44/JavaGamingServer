0509e5a5dda798d3d0a59d4626ac23efda276c0b
hola <hola@gmail.com>
2021-12-13 12:00:02 +0100

    applicationConfig class y primeras pruebas de funcionamiento

---
+++ src/java/restFULServer/PurchaseFacadeREST.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package restFULServer;
+
+import entities.IdPurchase;
+import entities.Purchase;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.PathSegment;
+
+/**
+ *
+ * @author ibai Arriola
+ */
+@Stateless
+@Path("entities.purchase")
+public class PurchaseFacadeREST extends AbstractFacade<Purchase> {
+
+    @PersistenceContext(unitName = "JavaGamingServerPU")
+    private EntityManager em;
+
+    private IdPurchase getPrimaryKey(PathSegment pathSegment) {
+        /*
+         * pathSemgent represents a URI path segment and any associated matrix parameters.
+         * URI path part is supposed to be in form of 'somePath;idGame=idGameValue;idClient=idClientValue'.
+         * Here 'somePath' is a result of getPath() method invocation and
+         * it is ignored in the following code.
+         * Matrix parameters are used as field names to build a primary key instance.
+         */
+        entities.IdPurchase key = new entities.IdPurchase();
+        javax.ws.rs.core.MultivaluedMap<String, String> map = pathSegment.getMatrixParameters();
+        java.util.List<String> idGame = map.get("idGame");
+        if (idGame != null && !idGame.isEmpty()) {
+            key.setIdGame(new java.lang.Integer(idGame.get(0)));
+        }
+        java.util.List<String> idClient = map.get("idClient");
+        if (idClient != null && !idClient.isEmpty()) {
+            key.setIdClient(new java.lang.Integer(idClient.get(0)));
+        }
+        return key;
+    }
+
+    public PurchaseFacadeREST() {
+        super(Purchase.class);
+    }
+
+    @POST
+    @Override
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public void create(Purchase entity) {
+        super.create(entity);
+    }
+
+    @PUT
+    @Path("{id}")
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public void edit(@PathParam("id") PathSegment id, Purchase entity) {
+        super.edit(entity);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void remove(@PathParam("id") PathSegment id) {
+        entities.IdPurchase key = getPrimaryKey(id);
+        super.remove(super.find(key));
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Purchase find(@PathParam("id") PathSegment id) {
+        entities.IdPurchase key = getPrimaryKey(id);
+        return super.find(key);
+    }
+
+    @GET
+    @Override
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public List<Purchase> findAll() {
+        return super.findAll();
+    }
+
+    @GET
+    @Path("{from}/{to}")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public List<Purchase> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
+        return super.findRange(new int[]{from, to});
+    }
+
+    @GET
+    @Path("count")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String countREST() {
+        return String.valueOf(super.count());
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+    
+}
