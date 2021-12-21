6f4f82f10cf9ea5492e4486e47cf139fb8fc2714
Alex Hurtado <lexurt@gmail.com>
2021-12-21 10:06:34 +0100

    Se ha añadido el Javadoc de las clases Hashing.java y RSACipher.java

--- src/java/security/Hashing.java
+++ src/java/security/Hashing.java
@@ -12,35 +12,49 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * Esta clase se utiliza para hashear las contraseñas de los usuarios
- * antes de almacenarla en la base de datos.
+ * Esta clase se utiliza para hashear las contraseñas de los usuarios antes de
+ * almacenarla en la base de datos.
+ *
  * @author Alex Hurtado, Ibai Arriola
  */
 public class Hashing {
+
     //Logger de clase Hashing
     private static final Logger LOG = Logger.getLogger(Hashing.class.getName());
 
     /**
-     * Este método se encarga de hashear la contraseña. Se utiliza también un salt para añadir más seguridad.
+     * Este método se encarga de hashear la contraseña. Se utiliza también un
+     * salt para añadir más seguridad.
+     *
      * @param passwordToHash Password que queremos hashear.
-     * @param salt  String hexadecimal aleatoria para añadir más seguridad al hashing.
-     * @return Password hasheada.
+     * @param salt String hexadecimal aleatoria para añadir más seguridad al
+     * hashing.
+     * @return String Password hasheada.
      */
     public static String getSHA256SecurePassword(String passwordToHash,
             String salt) {
-        String generatedPassword = null;
+        //String en la que se almacenará la password hasheada.
+        String hashedPassword = null;
+        //Instancia de la clase MessageDigest que se ocupará del hashing de la password.
         MessageDigest md;
         try
         {
+            //Obtenemos una instancia de la clase MessageDigest usando el algorítmo SHA-256.
             md = MessageDigest.getInstance("SHA-256");
+            //Para más seguridad añadimos un "salt" 
             md.update(salt.getBytes());
             byte[] bytes = md.digest(passwordToHash.getBytes());
-            generatedPassword = bytesToHexString(bytes);
+            /*
+            la password generada la convertimos a hexadecimal llamando al 
+            metodo bytesToHexString(bytes) tomando como parametro el array de
+            bytes hasheado.
+            */
+            hashedPassword = bytesToHexString(bytes);
         } catch (NoSuchAlgorithmException ex)
         {
             LOG.log(Level.SEVERE, null, ex.getMessage());
         }
-        return generatedPassword;
+        return hashedPassword;
     }
 
     /**
@@ -56,10 +70,11 @@ public class Hashing {
         sr.nextBytes(salt);
         return salt.toString();
     }
-    
+
     /**
-     * Este método convierte el array de bytes generado por la instancia de la clase MessageDigest
-     * en una cadena hexadecimal.
+     * Este método convierte el array de bytes generado por la instancia de la
+     * clase MessageDigest en una cadena hexadecimal.
+     *
      * @param bytes Array de bytes generado por la instancia de MessageDigest.
      * @return String hexadecimal.
      */
--- src/java/security/RSACipher.java
+++ src/java/security/RSACipher.java
@@ -24,14 +24,19 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 
 /**
- * Esta clase se ocupa del encriptado y desencriptado de la información
+ * Esta clase se ocupa del encriptado y desencriptado de la informacion
  * que le llega al servidor y que sale del mismo.
- * @author Alex Hurtado
+ * @author Alex Hurtado , Ibai Arriola
  */
 public class RSACipher {
     //Logger para la clase en encriptacion.
     private static final Logger LOG = Logger.getLogger(RSACipher.class.getName());
     
+    /**
+     * Este metodo sirve para convertir un array de bytes en una cadena haxedecimal.
+     * @param bytes
+     * @return String Cadena hexadecimal que representa al array de bytes pasado como parámetro.
+     */
     public static String bytesToHexString(byte[] bytes) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < bytes.length; i++)
@@ -42,7 +47,12 @@ public class RSACipher {
         return sb.toString();
     }
     
-    public static byte[] encrypt(byte[] plaintext) {
+    /**
+     * Este método encripta una cadena de texto pasada como parámetro.
+     * @param plainText Texto que se quiere encriptar.
+     * @return byte[] Array de bytes del texto encriptado.
+     */
+    public static byte[] encrypt(byte[] plainText) {
         Cipher cipher;
         byte[] bs = null;
         PublicKey key;
@@ -56,14 +66,19 @@ public class RSACipher {
             // Iniciamos el Cipher en ENCRYPT_MODE y le pasamos la clave publica
             cipher.init(Cipher.ENCRYPT_MODE, key);
             // Le decimos que cifre (método doFinal(mensaje))
-            bs = cipher.doFinal(plaintext);
+            bs = cipher.doFinal(plainText);
         } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException | IOException | InvalidKeySpecException ex)
         {
-            LOG.log(Level.SEVERE, null, ex);
+            LOG.log(Level.SEVERE, null, ex.getMessage());
         }
         return bs;
     }
 
+    /**
+     * Este método descifra el array de bytes encriptados que se pasa como parámetro.
+     * @param ciphertext Array de bytes encriptados.
+     * @return byte[] Array de bytes desencriptados.
+     */
     public static byte[] decrypt(byte[] ciphertext) {
         Cipher cipher;
         byte[] bs = null;
@@ -80,32 +95,53 @@ public class RSACipher {
             bs = cipher.doFinal(ciphertext);
         } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException | IOException | InvalidKeySpecException ex)
         {
-            LOG.log(Level.SEVERE, null, ex);
+            LOG.log(Level.SEVERE, null, ex.getMessage());
         }
         return bs;
     }
 
-    public static PrivateKey readPrivateKey(String filename) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
-        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(fileReader(filename));
+    /**
+     * Este método devuelve la clave privada almacenada en un fichero a partir de la ruta relativa del mismo.
+     * @param filePath Path relativo del fichero que contiene la clave.
+     * @return PrivateKey Clave privada almacenada el fichero.
+     * @throws IOException Excepción que se lanza si no existe el fichero.
+     * @throws NoSuchAlgorithmException Excepcion que se lanza si el algoritmo especificado en el getInstance(String algorithm) no existe.
+     * @throws InvalidKeySpecException  Excepción que se lanza si la especificación de la clave es inapropiada para el keySpec especificado.
+     */
+    public static PrivateKey readPrivateKey(String filePath) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(fileReader(filePath));
         KeyFactory keyFactory = KeyFactory.getInstance("RSA");
         return keyFactory.generatePrivate(keySpec);
     }
-
-    public static PublicKey readPublicKey(String filename) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
-        X509EncodedKeySpec publicSpec = new X509EncodedKeySpec(fileReader(filename));
+    
+    /**
+     * Este método devuelve la clave pública almacenada en un fichero a partir de la ruta relativa del mismo.
+     * @param filePath  Path relativo del fichero que contiene la clave.
+     * @return  PublicKey Clava privada almacenada en un fichero.
+     * @throws IOException Excepción que se lanza si no existe el fichero.
+     * @throws NoSuchAlgorithmException Excepcion que se lanza si el algoritmo especificado en el getInstance(String algorithm) no existe.
+     * @throws InvalidKeySpecException  Excepción que se lanza si la especificación de la clave es inapropiada para el keySpec especificado.
+     */
+    public static PublicKey readPublicKey(String filePath) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        X509EncodedKeySpec publicSpec = new X509EncodedKeySpec(fileReader(filePath));
         KeyFactory keyFactory = KeyFactory.getInstance("RSA");
         return keyFactory.generatePublic(publicSpec);
     }
 
+    /**
+     * Este método devuelve un array de bytes con el contenido del fichero cuyo path se ha pasado como parámetro.
+     * @param path Path relativo del fichero que se quiere leer.
+     * @return byte[] Array de bytes del contenido del fichero.
+     */
     private static byte[] fileReader(String path) {
         byte ret[] = null;
         File file = new File(path);
         try
         {
             ret = Files.readAllBytes(file.toPath());
-        } catch (IOException e)
+        } catch (IOException ex)
         {
-            LOG.log(Level.SEVERE, null, e);
+            LOG.log(Level.SEVERE, null, ex.getMessage());
         }
         return ret;
     }
